4ccff794c536547d766e986bcdc600bf07ac95bd
mokni20 <oussama.ahmed110@gmail.Com>
2022-04-08 17:45:45 +0100

    rout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { TabCommandeComponent } from './tab-commande/tab-commande.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    TabCommandeComponent
   ],
   imports: [
     BrowserModule,
